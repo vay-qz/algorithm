0ffe72cd033ea226016ebfd8a55b28a03a5de5b9
qiaozhe <qiaozhe@meituan.com>
2021-12-05 19:22:44 +0800

    贪心收尾

--- go/greedy/greedy.go
+++ go/greedy/greedy.go
@@ -23,3 +23,47 @@ func eraseOverlapIntervals(intervals [][]int) int {
 	}
 	return sum
 }
+
+func findMinArrowShots(points [][]int) int {
+	sort.Slice(points, func(i, j int) bool {
+		if points[i][1] < points[j][1] {
+			return true
+		}
+		return false
+	})
+	sum := 1
+	maxpoint := points[0][1]
+	for i := 1; i < len(points); i++ {
+		if points[i][1] > maxpoint {
+			sum++
+			maxpoint = points[0][1]
+		}
+	}
+	return sum
+}
+
+func lower(x ...int) int {
+	min := x[0]
+	for i := 1; i < len(x); i++ {
+		if x[i] < min {
+			min = x[i]
+		}
+	}
+	return min
+}
+
+func isSubsequence(s string, t string) bool {
+	if len(s) > len(t) {
+		return false
+	}
+	si := 0
+	for ti := 0; ti < len(t); ti++ {
+		if s[si] == t[ti] {
+			si++
+			if si == len(s) {
+				return true
+			}
+		}
+	}
+	return false
+}
